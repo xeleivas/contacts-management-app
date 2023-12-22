28563e17dea532217780c10edd91d059300aa8a8
Esteban Leivas <estebanleivas103@gmail.com>
2023-12-21 23:27:50 -0300

    refactor: group routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,13 +24,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
 
 /**
  * Contacts Routes
  */
-Route::get('/contacts', [ContactController::class, 'list'])->middleware('auth:sanctum');
-Route::post('/contacts/create', [ContactController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/contacts/{id}', [ContactController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->middleware('auth:sanctum');
+Route::prefix('contacts')->middleware('auth:sanctum')->controller(ContactController::class)->group(function () {
+    Route::get('/', 'list');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+});
