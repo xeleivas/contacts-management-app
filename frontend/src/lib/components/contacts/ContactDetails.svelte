b34e11af7a2872e8eeb772663dadce04cec51bf5
Esteban Leivas <estebanleivas103@gmail.com>
2023-12-23 23:12:31 -0300

    fix: details alignment on mobile

--- frontend/src/lib/components/contacts/ContactDetails.svelte
+++ frontend/src/lib/components/contacts/ContactDetails.svelte
@@ -10,7 +10,7 @@
 	};
 </script>
 
-<div class="w-full flex justify-center items-center mt-8 md:mt-20 md:ml-14">
+<div class="w-full flex justify-start items-center mt-8 md:justify-center md:mt-20 md:ml-14">
 	<div class="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-x-10">
 		<div class="w-full order-1 md:w-52 md:order-1">
 			<InfoDisplay label="Address" value={contact.address} />
